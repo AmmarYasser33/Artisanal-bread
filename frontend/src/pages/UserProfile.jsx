077c6c288f3515e2910a3c611572592919c64193
Ammar <ammar.yassr.33@gmail.com>
2024-11-11 21:26:28 +0200

    Add password management form to UserProfile page

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
 export default function UserProfile() {
   return (
-    <div className="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
+    <div className="space-y-10 sm:px-6 lg:col-span-9 lg:px-0">
       <form className="">
         <div className="shadow-lg sm:overflow-hidden sm:rounded-md">
           <div className="space-y-6 bg-white px-4 py-6 sm:p-6">
@@ -90,6 +90,77 @@ export default function UserProfile() {
                   id="delivery-address"
                   autoComplete="delivery-address"
                   // value="6th of October City, Giza, Egypt"
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md border border-transparent bg-primary-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <form className="">
+        <div className="shadow-lg sm:overflow-hidden sm:rounded-md">
+          <div className="space-y-6 bg-white px-4 py-6 sm:p-6">
+            <h3 className="mb-10 mt-2 text-xl font-semibold text-gray-900 sm:text-2xl">
+              Passwords
+            </h3>
+
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-4">
+                <label
+                  htmlFor="current-password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Current Password
+                </label>
+                <input
+                  type="password"
+                  name="current-password"
+                  id="current-password"
+                  autoComplete="current-password"
+                  placeholder="●●●●●●●●●"
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-4">
+                <label
+                  htmlFor="new-password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  New Password
+                </label>
+                <input
+                  type="password"
+                  name="new-password"
+                  id="new-password"
+                  autoComplete="new-password"
+                  placeholder="●●●●●●●●●"
+                  className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-4">
+                <label
+                  htmlFor="confirm-new-password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Confirm New Password
+                </label>
+                <input
+                  type="password"
+                  name="confirm-new-password"
+                  id="confirm-new-password"
+                  autoComplete="confirm-new-password"
+                  placeholder="●●●●●●●●●"
                   className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
                 />
               </div>
