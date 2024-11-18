fb40324ed2952142d37d786c1efd81e0f8058e05
Ammar <ammar.yassr.33@gmail.com>
2024-11-18 07:14:43 +0200

    Add directory creation for categories and products in static file utility

--- backend/utils/createStaticFiles.js
+++ backend/utils/createStaticFiles.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const rootPath = path.join(__dirname, "../", "uploads");
 const usersPath = path.join(rootPath, "users");
+const categoriesPath = path.join(rootPath, "categories");
+const productsPath = path.join(rootPath, "products");
 
 function ensureDirectories() {
   if (!fs.existsSync(rootPath)) {
@@ -13,6 +15,16 @@ function ensureDirectories() {
   if (!fs.existsSync(usersPath)) {
     fs.mkdirSync(usersPath);
     console.log("Created users directory");
+  }
+
+  if (!fs.existsSync(categoriesPath)) {
+    fs.mkdirSync(categoriesPath);
+    console.log("Created categories directory");
+  }
+
+  if (!fs.existsSync(productsPath)) {
+    fs.mkdirSync(productsPath);
+    console.log("Created products directory");
   }
 }
 
