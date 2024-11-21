7bde54c3c934b763d72527d62b9e1421167e2652
Ammar <ammar.yassr.33@gmail.com>
2024-11-21 16:11:18 +0200

    Add Redux store and user info management

---
+++ frontend/src/Store/index.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userInfoSlice from "./userInfo-slice";
+
+const store = configureStore({
+  reducer: {
+    userInfo: userInfoSlice.reducer,
+  },
+});
+
+export default store;
