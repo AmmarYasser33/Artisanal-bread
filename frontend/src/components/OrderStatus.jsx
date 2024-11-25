a577809a218a3d9f515f9d6c97996aedb1feeda8
Ammar <ammar.yassr.33@gmail.com>
2024-11-25 07:29:14 +0200

    Add OrderStatus component

---
+++ frontend/src/components/OrderStatus.jsx
@@ -0,0 +1,39 @@
+import {
+  IconTruckDeliveryOutline,
+  IconCheck2,
+  IconClose,
+  IconBxTimeFive,
+} from "../Icons";
+
+const capitalizeFirstLetter = (string) => {
+  if (!string) return "";
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export default function OrderStatus({ status }) {
+  const iconClass = "me-1 h-3 w-3";
+
+  const statusIcon = {
+    delivered: <IconCheck2 className={iconClass} />,
+    cancelled: <IconClose className={iconClass} />,
+    processing: <IconTruckDeliveryOutline className={iconClass} />,
+    pending: <IconBxTimeFive className={iconClass} />,
+  };
+
+  const statusColor = {
+    delivered: "bg-green-100 text-green-800",
+    cancelled: "bg-red-100 text-red-800",
+    processing: "bg-blue-100 text-blue-800",
+    pending: "bg-yellow-100 text-yellow-800",
+  };
+
+  return (
+    // <dd className="me-2 mt-1.5 inline-flex items-center rounded bg-yellow-100 px-2.5 py-0.5 text-xs font-medium text-yellow-800">
+    <dd
+      className={`me-2 mt-1.5 inline-flex items-center rounded px-2.5 py-0.5 text-xs font-medium ${statusColor[status]}`}
+    >
+      {statusIcon[status]}
+      {capitalizeFirstLetter(status)}
+    </dd>
+  );
+}
