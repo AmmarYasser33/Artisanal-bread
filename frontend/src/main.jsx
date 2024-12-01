b8c85fac376acec4445599afef6d76f8679d77e7
Ammar <ammar.yassr.33@gmail.com>
2024-12-01 06:51:45 +0200

    add React Query Devtools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 import store from "./Store/index";
 import { Provider } from "react-redux";
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </Provider>,
 );
