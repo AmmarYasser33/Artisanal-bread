47a209fecc19dfdc9b325812f53ff00c5b6bca84
Ammar <ammar.yassr.33@gmail.com>
2025-01-14 15:58:46 +0200

    add en-ar support for auth error messages

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,14 +1,16 @@
+import { useTranslation } from "react-i18next";
 import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function AdminRoute({ children }) {
+  const { t } = useTranslation();
   const role = JSON.parse(localStorage.getItem("role"));
 
   if (role === "admin") {
     return children;
   } else {
     toast.dismiss();
-    toast.error("Not authorized to access this page");
+    toast.error(t("auth.error"));
     return <Navigate to={"/login"} />;
   }
 }
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,13 +1,15 @@
+import { useTranslation } from "react-i18next";
 import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function ProtectedRoute({ children }) {
+  const { t } = useTranslation();
   const token = JSON.parse(localStorage.getItem("token"));
 
   if (token) {
     return children;
   } else {
-    toast.error("Please login to access this page");
+    toast.error(t("auth.login"));
     return <Navigate to={"/login"} />;
   }
 }
